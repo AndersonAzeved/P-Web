93d30320af78715922e0bafb11b3ad8f064097a2
Anderson Azevedo <andersonsilva14.2017@gmail.com>
2023-09-25 09:53:08 -0300

    up

--- Receitas/React com NextJS/next-03/pages/home.js
+++ Receitas/React com NextJS/next-03/pages/home.js
@@ -6,9 +6,9 @@ export default function Home(){
         <>
             <Nav/>
 
-            <div class={styles.div}>
-                <h1 class={styles.h1}>Neste site, é possível realizar buscas de filmes e séries</h1>
-                <p class={styles.p}>A partir da OMDb API, o site retorna busca relacionadas a filmes e séries, por meio do título e data.</p>
+            <div className={styles.div}>
+                <h1 className={styles.h1}>Neste site, é possível realizar buscas de filmes e séries</h1>
+                <p className={styles.p}>A partir da OMDb API, o site retorna busca relacionadas a filmes e séries, por meio do título e data.</p>
             </div>
         </>
     )
