056dde017b96bacfff2f299fb9fdee9db3ffe5b3
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-07 15:27:32 -0300

    next

--- Receitas/React com NextJS/next-05/pages/index.js
+++ Receitas/React com NextJS/next-05/pages/index.js
@@ -3,9 +3,9 @@ import styles from '../styles/home.module.css'
 export default function Home(){
     return (
         <>
-            <div class={styles.div}>
-                <h1 class={styles.h1}>Neste site, é possível realizar buscas de filmes e séries</h1>
-                <p class={styles.p}>A partir da OMDb API, o site retorna busca relacionadas a filmes e séries, por meio do título e data.</p>
+            <div className={styles.div}>
+                <h1 className={styles.h1}>Neste site, é possível realizar buscas de filmes e séries</h1>
+                <p className={styles.p}>A partir da OMDb API, o site retorna busca relacionadas a filmes e séries, por meio do título e data.</p>
             </div>
         </>
     )
