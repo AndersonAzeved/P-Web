7e82fa1d9e7e5420a125608aa83fb3da647b3740
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-10 18:47:20 -0300

    receita 1

--- Receitas/React com NextJS/next-01/components/nav.js
+++ Receitas/React com NextJS/next-01/components/nav.js
@@ -6,7 +6,28 @@ export default function Nav(){
     return (
         <ul className={styles.ul}>
             <li className={styles.li}>
-                <Link className={styles.link} href='/'>Home</Link>
+                <Link className={styles.link} href='https://receitas-next-anderson.vercel.app/'>Home</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-1.vercel.app/'>Receita 1</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-2.vercel.app/'>Receita 2</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-3.vercel.app/'>Receita 3</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-4.vercel.app/'>Receita 4</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-5.vercel.app/'>Receita 5</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-6.vercel.app/'>Receita 6</Link>
+            </li>
+            <li className={styles.li}>
+                <Link className={styles.link} href='https://next-receita-9.vercel.app/'>Receita 9</Link>
             </li>
         </ul>
     )
