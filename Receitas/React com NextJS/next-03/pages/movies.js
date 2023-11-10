30949a252152996768fe7c36ce77b0e6cbeaa507
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-10 19:29:15 -0300

    receita 3

--- Receitas/React com NextJS/next-03/pages/movies.js
+++ Receitas/React com NextJS/next-03/pages/movies.js
@@ -45,90 +45,11 @@ export default function MinhaPagina() {
 
     if((data == undefined || array == undefined) && fezBusca == 0){
         return (
-            <>
-                <Erro/>
-            </>
+            <Erro/>
         )
     }else{
         if(exibir == 'true'){
             return (
-                <>
-                    <Nav/>
-
-                    <div>
-                        <section class={styles.galeria}>
-                            {data?.Search?.map( (m) => ( 
-                                <div class={styles.poster}>
-                                    <img src={m.Poster} class={styles.imgPoster}/>
-                                    <p class={styles.textPoster}>{m.Title}, {m.Year}</p>
-                                </div>
-                            ))}            
-                        </section> 
-                    </div> 
-                </>
-            )
-        }else{
-            return (
-                <>
-                    <Nav/>
-
-                    <div>
-                        <section class={styles.galeria}>
-                            {array?.map( (m) => ( 
-                                <div class={styles.poster}>
-                                    <img src={m.Poster} class={styles.imgPoster}/>
-                                    <p class={styles.textPoster}>{m.Title}, {m.Year}</p>
-                                </div>
-                            ))}            
-                        </section> 
-                    </div>  
-                </>
-            )
-        }
-    }
-    
-    
-}
-
-export function Nav(){
-    return (
-        <>
-            <title>Movies</title>
-            <ul class={styles.ul}>
-                <li class={styles.li}>
-                    <Link class={styles.link} href='/home'>Home</Link>
-                </li>
-                <li>
-                    <Link class={styles.link} href='/busca'>Buscar</Link>
-                </li>
-            </ul>
-        </>
-
-    )
-}
-
-export function Erro(){
-    return (
-        <>
-            <Nav/>
-
-            <div class={styles.erro}>
-                <h1>Não foi possível realizar a busca</h1>
-                <p>Talvez os parâmetros de busca estejam errados, tente novamente</p>
-            </div>
-        </>
-    )
-}
-
-
-/*
-
-    if(exibir == 'true'){
-        alert("Exibir igual a true")
-        return(
-            <>
-                <Nav/>
-    
                 <div>
                     <section class={styles.galeria}>
                         {data?.Search?.map( (m) => ( 
@@ -138,15 +59,10 @@ export function Erro(){
                             </div>
                         ))}            
                     </section> 
-                </div>  
-            </>
-        )
-    }
-    else{
-        return(
-            <>
-                <Nav/>
-    
+                </div> 
+            )
+        }else{
+            return (
                 <div>
                     <section class={styles.galeria}>
                         {array?.map( (m) => ( 
@@ -157,55 +73,19 @@ export function Erro(){
                         ))}            
                     </section> 
                 </div>  
-            </>
-        )
+            )
+        }
     }
-*/
-
-
-
-
-
-/*import styles from '../styles/style.module.css'
-import { useRouter } from 'next/router'
-
-const apikey = '31c8f11c'
-
-export default function Movies({data}){
-   return (
-      <>
-      <title>
-         Movies
-      </title>
-         <div class={styles.divInicial}>
-            <div class={styles.titulo}>
-               Filmes & Séries
-            </div>
-            <section class={styles.galeria}>
-               {data?.Search.map( (m) => ( 
-                  <div class={styles.card}>
-                     <div class={styles.poster}>
-                        <img src={m.Poster} class={styles.imgPoster}/>
-                        {m.Title}, {m.Year}
-                     </div>
-                  </div>
-               )
-               )}                
-            </section> 
-         </div>  
-      </>
-      
-   )
+    
+    
 }
 
 
-export async function getServerSideProps(context){
-   const res = await fetch('http://www.omdbapi.com/?apikey=31c8f11c&s=spider')
-   //const res = await fetch("https://www.omdbapi.com/?apikey=31c8f11c&t=bee&y=2005")
-   const data = await res.json()
-   return {
-      props:{
-         data
-      }
-   }
-}*/
+export function Erro(){
+    return (
+        <div class={styles.erro}>
+            <h1>Não foi possível realizar a busca</h1>
+            <p>Talvez os parâmetros de busca estejam errados, tente novamente</p>
+        </div>
+    )
+}
