d14a14f76197b8da9b281df39bf5e1d71f123034
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-10 18:45:17 -0300

    receita final

--- Receitas/React com NextJS/next_final/pages/index.js
+++ Receitas/React com NextJS/next_final/pages/index.js
@@ -1,19 +1,10 @@
-import styles from '../styles/hino.module.css'
-import { Autor, Letra, Titulo } from "../lib/hino"
+import styles from '../styles/index.module.css'
 
 
 export default function Home(){
    return (
-      <div className={styles.hino}>
-         <h2 className={styles.viva}>
-            Viva Santana!
-         </h2>
-         <hr/>
-         <Titulo/>
-         <hr/>
-         <Autor/>
-         <hr/>
-         <Letra/>
+      <div className={styles.div}>
+         <p>Site criado para redirecionar as páginas, contendo as respostas de cada receita de Next.js, da disciplina de Progamação Web 2023.2</p>
       </div>
    )
 }
