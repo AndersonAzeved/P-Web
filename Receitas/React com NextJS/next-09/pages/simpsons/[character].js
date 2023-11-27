fdfb8f1d673ca5f9c3ea4de8b7cc4c6b9462580e
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-27 20:31:37 -0300

    exercício 1

--- Receitas/React com NextJS/next-09/pages/simpsons/[character].js
+++ Receitas/React com NextJS/next-09/pages/simpsons/[character].js
@@ -12,20 +12,25 @@ export default function TheMovie({movies}){
 }
 
 export async function getStaticPaths(){
+    const res = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes?count=5000000')
+    const data = await res.json()    
+    const paths = data.map((d) => ({
+        params: { character: (d.character.toLowerCase()).split(" ")[0] },
+      }));
     return {
-        paths:[
-            {params: {character: "homer"}},
+        paths,
+        /*paths:[
+            {params: {character: "mil"}},
             {params: {character: "bart"}},
             {params: {character: "moe"}},
             {params: {character: "lisa"}},
             {params: {character: "marge"}}
-        ],
+        ],*/
         fallback: true 
     }
 }
 
 export async function getStaticProps({ params }) {
-    console.log("getStaticProps")
     const res = await fetch(`https://thesimpsonsquoteapi.glitch.me/quotes?character=${params.character}`)
     const movies = await res.json();
     return {
