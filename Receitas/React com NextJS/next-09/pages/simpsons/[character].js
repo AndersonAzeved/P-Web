e483ec42eb618be3cd1c0f6bfde3c7880e774c37
Anderson Azevedo da Silva <104504983+AndersonAzeved@users.noreply.github.com>
2023-11-27 21:27:50 -0300

    exercício 2

--- Receitas/React com NextJS/next-09/pages/simpsons/[character].js
+++ Receitas/React com NextJS/next-09/pages/simpsons/[character].js
@@ -1,13 +1,39 @@
+import * as React from 'react';
+import AspectRatio from '@mui/joy/AspectRatio';
+import Button from '@mui/joy/Button';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import IconButton from '@mui/joy/IconButton';
+import Typography from '@mui/joy/Typography';
+
+
 export default function TheMovie({movies}){
     console.log(`Pré-renderizando ${movies}`)
     
     return (
         <div>
+            <Card sx={{ width: 300, height: 550, background: '#FADA5E'}}>
+                <AspectRatio minHeight="400px">
+                    <img style={{width: 250}} src={movies[0].image} alt={movies[0].character}/>
+                </AspectRatio>
+                <div>
+                    <Typography level="title-lg">{movies[0].character}</Typography>
+                    <Typography level="body-sm">{movies[0].quote}</Typography>
+                    <IconButton
+                    aria-label="bookmark Bahamas Islands"
+                    variant="plain"
+                    color="neutral"
+                    size="sm"
+                    sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}/>
+                </div>
+            </Card>
+        </div>
+        /*<div>
             <div>{movies[0].character} --- {movies[0].quote}</div>
             <div>
                 <img src={movies[0].image} wcharacterth="300" height="400"/>
             </div>
-        </div>   
+        </div>*/   
     ) 
 }
 
